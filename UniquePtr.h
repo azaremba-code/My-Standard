91be7a7b9fdc8754dc9d3ac65c58d99cd6c3f799
Adrian Zaremba <azaremba.code@gmail.com>
2026-01-18 23:28:01 -0500

    First draft of my unique_ptr.

---
+++ UniquePtr.h
@@ -0,0 +1,33 @@
+
+
+namespace mystd {
+	template <typename T>
+	class unique_ptr {
+	public:
+		unique_ptr(T* ptr = nullptr) : m_ptr {ptr}
+		{}
+
+		~unique_ptr() {	delete m_ptr; }
+
+		unique_ptr(unique_ptr) = delete;
+		operator=(unique_ptr) = delete;
+
+		unique_ptr(unique_ptr&& other) : unique_ptr {} {
+			swap(*this, other);
+		}
+
+		unique_ptr& operator=(unique_ptr&& other) {
+			swap(*this, other);
+			return *this;
+		}
+
+		friend swap(unique_ptr& left, unique_ptr& right) {
+			using std::swap; // enable ADL
+			swap(left.m_ptr, right.m_ptr);
+		}
+
+
+	private:
+		T* m_ptr {};
+	};
+}
