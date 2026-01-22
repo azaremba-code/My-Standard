b16b0b3a3499db9eee674411b2f335d457357e9c
Adrian Zaremba <azaremba.code@gmail.com>
2026-01-21 22:32:29 -0500

    Added move and changed swap to use move

--- Utility.h
+++ Utility.h
@@ -1,11 +1,18 @@
+#include "TypeTraits.h"
+
 class unique_ptr;
 
 namespace mystd {
 	template <typename T>
+	mystd::remove_reference_t<T>&& move(T&& x) {
+		return static_cast<mystd::remove_reference<T>&&>(x);
+	}
+
+	template <typename T>
 	constexpr void swap(T& a, T& b) {
-		T c = a;
-		a = b;
-		b = c;
+		T&& c = mystd::move(a);
+		a = mystd::move(b);
+		b = mystd::move(c);
 	}
 
 	template <>
